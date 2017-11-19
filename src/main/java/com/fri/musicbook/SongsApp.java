c57cfe9a0f0c76707967dbf154d6ca4e15537e6d
Jan Šubelj <jan.subelj010@gmail.com>
2017-11-19 11:46:57 +0100

    kumernetes deploy

--- src/main/java/com/fri/musicbook/SongsApp.java
+++ src/main/java/com/fri/musicbook/SongsApp.java
@@ -1,8 +1,11 @@
 package com.fri.musicbook;
 
+import com.kumuluz.ee.discovery.annotations.RegisterService;
+
 import javax.ws.rs.ApplicationPath;
 import javax.ws.rs.core.Application;
 
+@RegisterService
 @ApplicationPath("v1")
 public class SongsApp extends Application {
 }
