00a00b42f5f0e1c8c9be71f5545550df9b933aea
Jan Šubelj <jan.subelj010@gmail.com>
2018-01-05 22:10:35 +0100

    commiting the uncommited

--- src/main/resources/sql-scripts/init-songs.sql
+++ src/main/resources/sql-scripts/init-songs.sql
@@ -6,6 +6,10 @@ INSERT INTO songs (song_id, song_name, album_id) VALUES (5, 'U kamenu', 1);
 INSERT INTO songs (song_id, song_name, album_id) VALUES (6, 'Lesinari Rasirili Krila', 1);
 INSERT INTO songs (song_id, song_name, album_id) VALUES (7, 'Pevalo Hiljadu Maceva', 1);
 INSERT INTO songs (song_id, song_name, album_id) VALUES (8, 'Knez Visokih Suma', 1);
+INSERT INTO songs (song_id, song_name, album_id) VALUES (9, 'Neba nigde nema', 2);
+INSERT INTO songs (song_id, song_name, album_id) VALUES (10, 'Tragom Hromoga Vuka', 2);
+INSERT INTO songs (song_id, song_name, album_id) VALUES (11, 'I am immortal', 2);
+
 
 
 
