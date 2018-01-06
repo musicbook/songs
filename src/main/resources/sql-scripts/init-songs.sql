f4f8ac9e433d666263f3aa08fc116361f08a1a20
Jan Šubelj <jan.subelj010@gmail.com>
2018-01-06 17:12:22 +0100

    povečal db

--- src/main/resources/sql-scripts/init-songs.sql
+++ src/main/resources/sql-scripts/init-songs.sql
@@ -6,10 +6,25 @@ INSERT INTO songs (song_id, song_name, album_id) VALUES (5, 'U kamenu', 1);
 INSERT INTO songs (song_id, song_name, album_id) VALUES (6, 'Lesinari Rasirili Krila', 1);
 INSERT INTO songs (song_id, song_name, album_id) VALUES (7, 'Pevalo Hiljadu Maceva', 1);
 INSERT INTO songs (song_id, song_name, album_id) VALUES (8, 'Knez Visokih Suma', 1);
+
 INSERT INTO songs (song_id, song_name, album_id) VALUES (9, 'Neba nigde nema', 2);
 INSERT INTO songs (song_id, song_name, album_id) VALUES (10, 'Tragom Hromoga Vuka', 2);
 INSERT INTO songs (song_id, song_name, album_id) VALUES (11, 'I am immortal', 2);
 
+INSERT INTO songs (song_id, song_name, album_id) VALUES (12, 'Ma Baker', 3);
+INSERT INTO songs (song_id, song_name, album_id) VALUES (13, 'Love for Sale', 3);
+INSERT INTO songs (song_id, song_name, album_id) VALUES (14, 'Belfast', 3);
 
 
+INSERT INTO songs (song_id, song_name, album_id) VALUES (15, 'Nightfall to Venus', 4);
+INSERT INTO songs (song_id, song_name, album_id) VALUES (16, 'Rasputin', 4);
+INSERT INTO songs (song_id, song_name, album_id) VALUES (17, 'Painter Man', 4);
 
+
+INSERT INTO songs (song_id, song_name, album_id) VALUES (18, 'Raise Your Head', 5);
+INSERT INTO songs (song_id, song_name, album_id) VALUES (19, 'There´s No Time', 5);
+INSERT INTO songs (song_id, song_name, album_id) VALUES (20, 'Social Disease', 5);
+
+INSERT INTO songs (song_id, song_name, album_id) VALUES (21, 'Dark Path', 6);
+INSERT INTO songs (song_id, song_name, album_id) VALUES (22, 'Tank of Butane', 6);
+INSERT INTO songs (song_id, song_name, album_id) VALUES (23, 'July Lullaby', 6);
